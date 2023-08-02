a0a0e6133edd74f7cbc7b510c9abfe5210352b5c
mohan <mohan@gmail.com>
2023-08-02 13:24:55 +0530

    updated animation componenet

--- src/components/FadeInAnimation.jsx
+++ src/components/FadeInAnimation.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const FadeInAnimation = (props) => {
-    const [isVisible, setVisible] = React.useState(true);
-    const domRef = React.useRef();
-    React.useEffect(() => {
-      const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => setVisible(entry.isIntersecting));
-      });
-      observer.observe(domRef.current);
-      return () => observer.unobserve(domRef.current);
-    }, []);
-    return (
-      <div
-        className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
-        ref={domRef}
-      >
-        {props.children}
-      </div>
-      
-    );
-}
+  const [isVisible, setVisible] = React.useState(true);
+  const domRef = React.useRef();
 
-export default FadeInAnimation
+  React.useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+    const currentRef = domRef.current; 
+    return () => {
+      observer.unobserve(currentRef); 
+    };
+  }, []);
+
+  return (
+    <div className={`fade-in-section ${isVisible ? "is-visible" : ""}`} ref={domRef}>
+      {props.children}
+    </div>
+  );
+};
+
+export default FadeInAnimation;
